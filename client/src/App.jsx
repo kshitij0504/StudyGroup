227a1bd31b16016244561ed4063c4fad76ce9b16
kshitij0504 <kshitijoza04@gmail.com>
2024-07-22 23:36:44 +0530

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import Header from "./Components/Header";
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/SignIn" element={<SignIn />} />

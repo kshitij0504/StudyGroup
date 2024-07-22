a88df09889232454f117548ddd4a859e5339e2ec
kshitij0504 <kshitijoza04@gmail.com>
2024-07-22 22:20:16 +0530

    Install react

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const router = require("./router/index");
+const cookieParser = require('cookie-parser')
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is Running",
+  });
+});
+
+// api endpoints
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
